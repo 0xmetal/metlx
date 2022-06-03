096250e99774e97321b2b84302952c397814653d
metlx <metlx>
2022-06-03 00:53:35 -0400

    slight redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,114 @@
-document.addEventListener("DOMContentLoaded",function(){new SweetScroll({}),particlesJS("particles-js",{particles:{number:{value:13,density:{enable:!0,value_area:800}},color:{value:"#000000"},shape:{type:"triangle",stroke:{width:3,color:"#000000"},triangle:{nb_sides:3},image:{src:"img/github.svg",width:100,height:100}},opacity:{value:.5,random:!1,anim:{enable:!1,speed:1,opacity_min:.1,sync:!1}},size:{value:6,random:!0,anim:{enable:!1,speed:19.18081918081918,size_min:.1,sync:!1}},line_linked:{enable:!0,distance:150,color:"#000000",opacity:.4,width:3},move:{enable:!0,speed:3,direction:"none",random:!0,straight:!1,out_mode:"out",bounce:!1,attract:{enable:!1,rotateX:600,rotateY:1200}},nb:80},interactivity:{detect_on:"canvas",events:{onhover:{enable:!1,mode:"grab"},onclick:{enable:!0,mode:"push"},resize:!0},modes:{grab:{distance:400,line_linked:{opacity:1}},bubble:{distance:400,size:40,duration:2,opacity:8,speed:3},repulse:{distance:200,duration:.4},push:{particles_nb:4},remove:{particles_nb:2}}},retina_detect:!0})},!1);
+document.addEventListener("DOMContentLoaded", function() {
+    new SweetScroll({}),
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 50,
+                density: {
+                    enable: !0,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#000000"
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 6,
+                    color: "#000"
+                },
+                circle: {
+                    nb_sides: 3
+                },
+                image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100
+                }
+            },
+            opacity: {
+                value: .5,
+                random: !1,
+                anim: {
+                    enable: !1,
+                    speed: 3,
+                    opacity_min: .1,
+                    sync: !1
+                }
+            },
+            size: {
+                value: 3,
+                random: !0,
+                anim: {
+                    enable: !1,
+                    speed: 1918081918081918,
+                    size_min: .1,
+                    sync: !1
+                }
+            },
+            line_linked: {
+                enable: !0,
+                distance: 150,
+                color: "#000000",
+                opacity: .4,
+                width: 3
+            },
+            move: {
+                enable: !0,
+                speed: 3,
+                direction: "none",
+                random: !0,
+                straight: !1,
+                out_mode: "out",
+                bounce: !1,
+                attract: {
+                    enable: !1,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            },
+            nb: 80
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: !1,
+                    mode: "grab"
+                },
+                onclick: {
+                    enable: !0,
+                    mode: "push"
+                },
+                resize: !0
+            },
+            modes: {
+                grab: {
+                    distance: 400,
+                    line_linked: {
+                        opacity: 1
+                    }
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
+                },
+                repulse: {
+                    distance: 200,
+                    duration: .4
+                },
+                push: {
+                    particles_nb: 4
+                },
+                remove: {
+                    particles_nb: 2
+                }
+            }
+        },
+        retina_detect: !0
+    })
+}, !1);
